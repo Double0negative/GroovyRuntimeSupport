be9a733efb235ce543a1e0bdf871f60a5fd8cc07
Double0negative <Double0negative@mc-sg.org>
2015-01-31 05:47:11 -0500

    init commit

---
+++ src/main/java/org/mcsg/groovy/GroovyRuntimeSupport.groovy
@@ -0,0 +1,14 @@
+package org.mcsg.groovy
+
+import org.bukkit.Bukkit
+import org.bukkit.craftbukkit.libs.com.google.gson.Gson
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
+
+class GroovyRuntimeSupport extends JavaPlugin{
+
+	void onEnable(){
+		println "Groovy Runtime Support has been loaded"
+	}
+
+}
